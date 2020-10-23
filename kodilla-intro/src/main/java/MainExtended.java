7d607fc450d6f8d0b758e0693a9df485126a57d3
Maria Kowalska <maruskowa@gmail.com>
2020-10-23 18:14:14 +0200

    version 23.10.2020

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String seasonOfTheYear = "Autumn";
+        System.out.println(seasonOfTheYear);
+        int year = 2020;
+        System.out.println(year);
+        double height = 1.74;
+        System.out.println(height);
+        char firstSurnameLetter = 'K';
+        System.out.println(firstSurnameLetter);
+        boolean isGoodMommy = true;
+        System.out.println(isGoodMommy);
+        int myAge = 32;
+        System.out.println(myAge);
+        String favouriteCuisine = "Italian";
+        System.out.println(favouriteCuisine);
+    }
+}
