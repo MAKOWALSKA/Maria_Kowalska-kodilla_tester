f608ee76e125e8cfb3ca3c8488707044ccbb312d
Maria Kowalska <maruskowa@gmail.com>
2020-10-23 15:58:43 +0200

    version 22.10.2020

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Maria"); // [4]
+        System.out.println(32); // [5]
+        System.out.println('K'); // [6]
+
+    }
+}
