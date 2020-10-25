3c84d2087471045d4c90bd48da49d3f422d4c67f
Maria Kowalska <maruskowa@gmail.com>
2020-10-25 17:42:35 +0100

    version 25.10.2020

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+    public static boolean leap(int year) {
+        return ((year % 4 == 0) && (year % 100 == 0)) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        int year;
+        year = 2020;
+
+        if (leap(year)){
+            System.out.println("rok jest przestepny.");
+        }
+        else {
+            System.out.println("rok nie jest przestepny.");
+        }
+    }
+}
---
+++ src/main/java/KolejnaKlasa.java
@@ -0,0 +1,5 @@
+public class KolejnaKlasa { // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from NextClass!"); // [3]
+    }
+}
