0ea9641491614451be51897b2aea08adc1a619a2
MAKOWALSKA <73294637+MAKOWALSKA@users.noreply.github.com>
2020-12-05 17:31:10 +0100

    task-3.1 dziedziczenie w praktyce poprawione (#4)

--- kodilla-basic-tests/src/main/java/kodilla/inheritance/Linux.java
+++ kodilla-basic-tests/src/main/java/kodilla/inheritance/Linux.java
@@ -5,12 +5,12 @@ public class Linux extends OperatingSystem {
     public Linux(int year) {
         super(year);
     }
-
+    @Override
     public void turnOn() {
-        System.out.println("System is turned on");
+        System.out.println("Linux is turned on");
     }
     @Override
     public void turnOff(){
-        System.out.println("System restarted and turned off");
+        System.out.println("Linux restarted and turned off");
     }
 }
