44bb4347ec378ce648440df41653a481c6548881
Maria Kowalska <maruskowa@gmail.com>
2020-12-18 20:06:34 +0100

    task-5.4 księgoźbiór poprawione

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -8,19 +8,13 @@ public class BookManager {
 
     public static Book createBook(String title, String author) {
 
-        books.add(new Book("La Divina Comedia", "Dante Alighieri"));
-        books.add(new Book("Il Canzoniere", "Francesco Petrarca"));
-        books.add(new Book("Decamerone", "Giovanni Boccaccio"));
-
         for (Book book : books) {
-            if (book.getTitle().equals(title) && book.getAuthor().equals(author)) {
+            if (book.getAuthor().equals(author) && book.getTitle().equals(title)) {
                 return book;
             }
-
         }
-        return new Book(title, author);
+        Book newBook = new Book(author, title);
+        books.add(newBook);
+        return newBook;
     }
 }
-//wiem, ze nie jest ok - znalazlam info a propo hashset na testers help, dodalam hashset i ksiazki
-//trzeba dodac metode sprawdzajaca czy obiekt juz istnieje... boolean contains(Object o)? jesli tak, to nie wiem, jak to zrobic. help, pls:)
-
