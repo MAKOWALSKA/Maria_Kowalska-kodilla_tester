6403fd230c08f580674794ee865e68db6bcfc118
MAKOWALSKA <73294637+MAKOWALSKA@users.noreply.github.com>
2021-01-12 09:52:55 +0100

    task-6.4 obsługa błędów (#20)
    * task-6.4 obsługa błędów

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("52"));
+        warehouse.addOrder(new Order("12"));
+        warehouse.addOrder(new Order("4"));
+        warehouse.addOrder(new Order("32"));
+        try {
+            warehouse.getOrder("46");
+            System.out.println("Order confirmed");
+        } catch (OrderDoesntExistException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
