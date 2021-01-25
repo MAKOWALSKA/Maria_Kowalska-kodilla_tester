08e7e7696cd9fcf5edbec59f8b95ae0afda1a7cf
Maria Kowalska <maruskowa@gmail.com>
2021-01-25 23:16:37 +0100

    task-10.3 rozbudowa API

--- kodilla-rest-api/src/main/java/com/kodilla/rest/service/BookService.java
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/service/BookService.java
@@ -18,4 +18,8 @@ class BookService {
     public void addBook(BookDto bookDto) {
         books.add(bookDto);
     }
+
+    public void removeBook(BookDto bookDto){
+        books.remove(bookDto);
+    }
 }
