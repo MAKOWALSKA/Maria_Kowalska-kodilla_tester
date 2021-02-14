64946a7f3117210264950c207632cef0623eeb3e
Maria Kowalska <maruskowa@gmail.com>
2021-02-14 21:41:16 +0100

    task 13.4 Ćwiczymy selektory CSS

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,24 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl");
+
+        driver.findElement(By.xpath("//button[@data-role=\"accept-consent\"]")).click();
+
+        WebElement inputField = driver.findElement(By.xpath("/html/body/div[3]/div[5]/header/div/div/div[1]/div/form/input"));
+        inputField.sendKeys("Mavic mini");
+        inputField = driver.findElement(By.xpath("/html/body/div[3]/div[5]/header/div/div/div[1]/div/form/div[2]/div/select"));
+        inputField.sendKeys("Elektronika ");
+        inputField = driver.findElement(By.xpath("/html/body/div[3]/div[5]/header/div/div/div[1]/div/form/button"));
+        inputField.click();
+
+    }
+}
