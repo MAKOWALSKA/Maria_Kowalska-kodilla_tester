293db55546ee09c3cf199804593b53083822ccb3
Maria Kowalska <maruskowa@gmail.com>
2021-02-21 17:20:03 +0100

    modul 14.3

--- kodilla-cucumber/src/test/java/TestRunner.java
+++ kodilla-cucumber/src/test/java/TestRunner.java
@@ -1,4 +1,3 @@
-package seasonchecker;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -6,7 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources"}
+        features = {"src/test/resources/wallet/cash_withdrawal.feature"}
 )
 public class TestRunner {
 }
