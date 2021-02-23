e45c61be45c49d0678f8e609b16254f14bdadd5d
MAKOWALSKA <73294637+MAKOWALSKA@users.noreply.github.com>
2021-02-23 19:21:32 +0100

    task-7.4 zamówienia (#40)

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -0,0 +1,46 @@
+package com.kodilla.execution_model.homework;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Shop {
+    private final List<Order> orders = new ArrayList<>();
+
+    public List<Order> getOrder() {
+        return orders;
+    }
+
+    public void addOrder(Order order) {
+        this.orders.add(order);
+    }
+
+    public List<Order> getOrderBtwTwoDates(LocalDate from, LocalDate to) {
+        if (to.isBefore(from))
+            return Collections.emptyList();
+        return orders.stream()
+                .filter(order -> order.getDate().isAfter(from) && order.getDate().isBefore(to))
+                .collect(Collectors.toList());
+    }
+
+    public List<Order> getOrderFromValueRange(double min, double max) {
+        if (orders.size() == 0)
+            return Collections.emptyList();
+        return orders.stream()
+                .filter(order -> order.getValue() >= min && order.getValue() <= max)
+                .collect(Collectors.toList());
+    }
+
+    public int getShopSize() {
+        return orders.size();
+    }
+
+    public double getTotalValue() {
+        return orders.stream()
+                .map(Order::getValue)
+                .mapToDouble(a -> a)
+                .sum();
+    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -0,0 +1,92 @@
+package com.kodilla.execution_model.homework;
+
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShopTestSuite {
+    Shop shop = new Shop();
+    Order one = new Order("Angelo Comunello", LocalDate.of(2020, 12, 31), 250);
+    Order two = new Order("Radoslaw Jaskot", LocalDate.of(2019, 4, 2), 100);
+    Order three = new Order("Michal Marzec", LocalDate.of(2018, 2, 27), 80);
+
+    @BeforeAll
+    public static void IntroMessage() {
+        System.out.println("Starting testing.");
+    }
+
+    @BeforeEach
+    public void addOrder() {
+        shop.addOrder(one);
+        shop.addOrder(two);
+        shop.addOrder(three);
+    }
+
+    @Test
+    public void addOrderTest() {
+        int numberOfOrders = shop.getShopSize();
+        assertEquals(3, numberOfOrders);
+    }
+
+    @Test
+    //są zamówienia
+    public void existingOrderTotalValueTest() {
+        shop.getTotalValue();
+        assertEquals(430, shop.getTotalValue(), 0.01);
+    }
+
+    @Test
+    //nie ma zamowien (sum 0)
+    public void totalValueWhenNoOrdersMadeTest() {
+        shop.getOrder().clear();
+        assertEquals(0, shop.getTotalValue());
+    }
+
+    @Test
+    public void returnNumberOfOrdersBetweenTwoDates() {
+        int list = shop.getOrderBtwTwoDates
+                (LocalDate.of(2020, 12, 1),
+                        LocalDate.of(2021, 1, 1)).size();
+        assertEquals(1, list);
+    }
+
+    @Test
+    public void returnEmptyListWhenDatesAreIncorrect() {
+        List<Order> orders = shop.getOrderBtwTwoDates
+                (LocalDate.of(2021, 1, 1),
+                        LocalDate.of(2020, 12, 1));
+        assertEquals(Collections.emptyList(), orders);
+    }
+
+    @Test
+    public void getNumberOfOrdersInValueRangeTest() {
+        List<Order> orders = shop.getOrderFromValueRange(90, 260);
+        assertEquals(2, orders.size());
+    }
+
+    @Test
+    public void returnEmptyListWhenValuesAreIncorrect() {
+        List<Order> orders = shop.getOrderFromValueRange(1000, 500);
+        assertEquals(Collections.emptyList(), orders);
+    }
+
+    @Test
+    public void returnEmptyListWhenOrdersOutOfValueRange() {
+        List<Order> orders = shop.getOrderFromValueRange(500, 1000);
+        assertEquals(Collections.emptyList(), orders);
+    }
+
+    @AfterEach
+    public void resetValues() {
+        System.out.println("Next test.");
+    }
+
+    @AfterAll
+    public static void GoodByeMessage() {
+        System.out.println("Testing completed.");
+    }
+}
