66375624f6e4e693b3aa7a71b3a093d96330059f
MAKOWALSKA <73294637+MAKOWALSKA@users.noreply.github.com>
2021-02-24 18:48:39 +0100

    task-14.2 Używamy Cucumbera (#47)

---
+++ kodilla-cucumber/src/main/java/homework/FizzBuzzChecker.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class FizzBuzzChecker {
+    public String fizzBuzzChecker(int number) {
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            return "FizzBuzz";
+        } else if ((number % 3 == 0)) {
+            return "Fizz";
+        } else if ((number % 5 == 0)) {
+            return "Buzz";
+        } else return "None";
+    }
+}
---
+++ kodilla-cucumber/src/test/java/homework/FizzBuzzTestRunner.java
@@ -0,0 +1,12 @@
+package homework;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/fizz_or_buzz.feature"}
+)
+public class FizzBuzzTestRunner {
+}
