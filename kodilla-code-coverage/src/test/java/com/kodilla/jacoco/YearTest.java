60ba900741de27f9500e16ed76ea528881be5eed
Maria Kowalska <maruskowa@gmail.com>
2021-02-28 11:15:08 +0100

    task-15.2 praca z JaCoCo i SonarLint

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -2,6 +2,7 @@ package com.kodilla.jacoco;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class YearTest {
@@ -10,6 +11,42 @@ public class YearTest {
 
         //given
         Year year = new Year(1600);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy4Or400() {
+        //given
+        Year year = new Year(2021);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy400() {
+        //given
+        Year year = new Year(2021);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4ButNotBy100() {
+        //given
+        Year year = new Year(2024);
 
         //when
         boolean isLeapYear = year.isLeap();
